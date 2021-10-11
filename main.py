24056edf6869eb8d8bbaf34a5d28d5e54e377ec9
rol-x <pl.charlessummers@gmail.com>
2021-10-11 16:46:15 +0200

    Added docker-compose file

--- main.py
+++ main.py
@@ -13,12 +13,7 @@ from etl.handlers.web_handler import (add_sellers_from_set,
                                       is_valid_card_page, realistic_pause,
                                       restart_webdriver, urlify)
 
-# TODO: Look into handling of wild Firefox processes.
-# TODO: Change singular to plural in entities use, not in model.
-
-# Docker containers startup commands
-# docker run --rm --network the-gathering -v gathering-data:/data -v gathering-logs:/logs the-gathering
-# docker run -d --network the-gathering --network-alias=webdriver -p 4444:4444 selenium/standalone-firefox
+# TODO: Change singular to plural in entities use, not in model
 
 # Main function
 if __name__ == "__main__":
