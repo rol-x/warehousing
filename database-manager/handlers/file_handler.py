9bf7bfb3fbf1542bf7cbb1f07a4eb263e7019ed9
rol-x <pl.charlessummers@gmail.com>
2021-10-17 14:11:17 +0200

    Database manager development

--- database-manager/handlers/file_handler.py
+++ database-manager/handlers/file_handler.py
@@ -1,9 +1,73 @@
 import os
+import time
 
 import pandas as pd
 from checksumdir import dirhash
 
 from handlers.log_handler import log
+
+
+# Detect changes in data directory based on calculated checksums
+def register_change():
+    last_hash = open("data-checksum.sha1", "r").readline()
+    this_hash = generate_data_hash()
+    change_registered = False
+
+    # Timeout after update over four (4) hours
+    while ~change_registered:
+        # Every thirty (30) minutes check whether the two hashes differ
+        while this_hash == last_hash:
+            log("Checksums checked - no changes detected")
+            time.sleep(60 * 30)
+            this_hash = generate_data_hash()
+        log("Change in data files detected")
+
+        # Every thirty (30) seconds check whether the update flag is active
+        start = time.time()
+        while open("./data/update_flag", "r").readline() == '1':
+            log("Update flag active")
+            if time.time() - start > 60 * 60 * 4:
+                log("Waiting for the update to end timed out")
+                reset_update_flag()
+                break
+            time.sleep(30)
+        log("Update flag not active")
+        if time.time() - start < 60 * 60 * 4:
+            change_registered = True
+
+    # On finished update or data migration the files are static
+    this_hash = generate_data_hash()
+    log("Changes ready to commence")
+    log("Old hash: " + last_hash)
+    log("New hash: " + this_hash)
+    log("Saving new hash to the file")
+    save_hash(this_hash)
+
+
+# Return generated hash based on the contents of data directory
+def generate_data_hash():
+    return str(dirhash('./data', 'sha1'))
+
+
+# Save given data hash to an external file
+def save_hash(hash):
+    with open('data-checksum.sha1', 'w+') as hash_file:
+        hash_file.write(hash)
+
+
+# Set up the file with information about ongoing update.
+def set_update_flag():
+    '''Set up the file with information about ongoing update.'''
+    update_flag = open('./data/update_flag', 'w')
+    update_flag.write('1')
+    update_flag.close()
+
+
+# Update the flag about the end of the update
+def reset_update_flag():
+    update_flag = open('./data/update_flag', 'w')
+    update_flag.write('0')
+    update_flag.close()
 
 
 # Try to securely load a dataframe from a .csv file.
@@ -16,14 +80,3 @@ def secure_load_df(entity_name):
         log(parser_err)
         os.system.exit("Importing data from csv failed - aborting.\n")
     return df
-
-
-# Return generated hash based on the contents of data directory
-def generate_data_hash():
-    return str(dirhash('./data', 'sha1'))
-
-
-# Save given data hash to an external file
-def save_hash(hash):
-    with open('data-checksum.sha1', 'w+') as hash_file:
-        hash_file.write(hash)
--- database-manager/main.py
+++ database-manager/main.py
@@ -1,39 +1,33 @@
 import time
 
-from handlers.file_handler import generate_data_hash, save_hash
+from handlers.file_handler import register_change
 from handlers.log_handler import log
 
-
-# Detect changes in data directory based on calculated checksums
-def register_change():
-    last_hash = open("data-checksum.sha1", "r").readline()
-    this_hash = generate_data_hash()
-
-    # Every thirty minutes check whether the two hashes differ
-    while(this_hash == last_hash):
-        time.sleep(60 * 30)
-        this_hash = generate_data_hash()
-
-    # Every thirty seconds check whether the update flag is active
-    log("Change in data files detected")
-    while(open("./data/update_flag", "r").readline() == '1'):
-        time.sleep(30)
-
-    # On finished update or data migration the files are static
-    this_hash = generate_data_hash()
-    log("Changes ready to commence")
-    log("Old hash: " + last_hash)
-    log("New hash: " + this_hash)
-    log("Saving new hash to the file")
-    save_hash(this_hash)
-    return
-
-
 # TODO: Figure out whether sleep is working properly, or possible alternatives
+# TODO: Fill out main function
 
 # Main function
 if __name__ == '__main__':
     time.sleep(60)
     while True:
+        # Wait until change in files is detected and any updates are finished
         register_change()
+
+        # Create a connection to the database
         log("Database update here")
+
+        # Verify the integrity of the database
+
+        # Load the files into pandas dataframes
+
+        # Analyze the data validity of the data
+
+        # Analyze how the data relates to the database
+
+        # Send and update the data in the database
+
+        # Verify the integrity of the database
+
+        # Close the database connection
+
+        # End program execution
