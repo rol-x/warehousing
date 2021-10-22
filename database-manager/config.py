09f2db63f10fdc1bcf5852eead01c70ca9e50bf5
rol-x <pl.charlessummers@gmail.com>
2021-10-22 13:00:15 +0200

    Module compliance

--- database-manager/config.py
+++ database-manager/config.py
@@ -1,11 +1,11 @@
 """Store and initialize global configuration of the program."""
 
-# Variables for user custom program configuration
-
 # Variables connected to this single run of the code
-CONTAINER_DELAY = 20
-NEW_CHECKSUM = ''
 DATABASE = None
+NEW_CHECKSUM = ''
+LOG_FILENAME = 'other.log'
+DAILY_LOGNAME = 'other_daily.log'
 
 # Fixed variables
 NAME = 'database-manager'
+CONTAINER_DELAY = 20
