29092a439a290f78c6171783684e74c6161764ff
rol-x <pl.charlessummers@gmail.com>
2021-10-22 13:00:00 +0200

    Config reorder

--- data-gathering/config.py
+++ data-gathering/config.py
@@ -1,22 +1,22 @@
 """Store and initialize global configuration of the program."""
 
 # Variables for user custom program configuration
-WAIT_COEF = 3.3
-BUTTON_TIMEOUT = 120.0
-EXPANSION_NAME = 'Battlebond'
-MAX_TRIES = 3
 START_FROM = 1
-WEBDRIVER_HOSTNAME = 'firefox_webdriver'
 FORCE_UPDATE = False
+EXPANSION_NAME = 'Battlebond'
+BUTTON_TIMEOUT = 120.0
+WAIT_COEF = 3.3
+MAX_TRIES = 3
 
 # Variables connected to this single run of the code
+FILE_PART = 1
+THIS_DATE_ID = 0
 LOG_FILENAME = 'other.log'
 DAILY_LOGNAME = 'other_daily.log'
-THIS_DATE_ID = 0
-FILE_PART = 1
-CONTAINER_DELAY = 10
 
 # Fixed variables
+NAME = 'data-gathering'
 BASE_URL = 'https://www.cardmarket.com/en/Magic/Products/Singles/'
 USERS_URL = 'https://www.cardmarket.com/en/Magic/Users/'
-NAME = 'data-gathering'
+WEBDRIVER_HOSTNAME = 'firefox_webdriver'
+CONTAINER_DELAY = 10
