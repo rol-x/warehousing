c6c3eba29bda61beaaa9ca5fec518a3cb01a2279
rol-x <pl.charlessummers@gmail.com>
2021-10-24 13:43:00 +0200

    Database handling

---
+++ database-manager/handlers/db_handler.py
@@ -0,0 +1,70 @@
+import config
+import mysql.connector
+import time
+
+from services.logs_service import log
+
+
+def connect_to_database():
+    try:
+        config.DATABASE = mysql.connector.connect(
+                            host="mysql_database",
+                            user="database-manager",
+                            password="h4Rd_p4sSW0rD")
+    except Exception as exception:
+        log(exception)
+        raise SystemExit from exception
+    log("Database connection established")
+
+
+def run_query(query, silent=False):
+    log(f"Executing query: {query}")
+    with config.DATABASE.cursor() as cursor:
+        cursor.execute(query)
+        results = cursor.fetchall()
+        if not silent:
+            for row in results:
+                log(row)
+    return results
+
+
+def run_queries(queries, silent=True):
+    for query in queries:
+        run_query(query, silent)
+        time.sleep(3)
+
+
+def setup_database():
+    run_query("SHOW DATABASES")
+    run_queries(["USE gathering",
+                 "CREATE TABLE IF NOT EXISTS card( \
+                            id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, \
+                            name VARCHAR(50), \
+                            expansion_name VARCHAR(20), \
+                            rarity VARCHAR(20))",
+                 "INSERT INTO card (name, expansion_name, rarity) \
+                     VALUES ('Test card', 'Battleborn', 'Mystic')",
+                 "SELECT * FROM card"])
+
+
+def run_update():
+    # Create a connection to the database
+    log("Database update here")
+
+    # Date        ->  every time, check the delta, update
+    # Card        ->  check if needed, check the delta, update
+    # Seller      ->  every time, check the delta, update
+    # Card stats  ->  every time, slice current day, create or update on these
+    # Sale offer  ->  every time, slice current day and card, create or update
+
+    # Date OK  \                        Date OK   \
+    #           -->  Card stats         Seller OK  -->  Sale offer
+    # Card OK  /                        Card OK   /
+
+    # Date FAIL or Card FAIL  -->  ROLLBACK {Faulty}, Card stats, Sale offer
+    # Seller FAIL  -->  ROLLBACK Seller, Sale offer
+    # {Any} FAIL  -->  ROLLBACK {Faulty}
+
+
+def close_connection():
+    config.DATABASE.close()
--- database-manager/main.py
+++ database-manager/main.py
@@ -1,30 +1,58 @@
 import time
 
-from handlers.file_handler import register_change
-from handlers.log_handler import log, setup_logging
+import config
+import services.flags_service as flags
+from handlers import db_handler, file_handler
+from services.logs_service import log, setup_logs, setup_main_logfile
 
 # TODO: Write simple database connection, create empty tables if there are none
 # TODO: Check local data against database contents & decide how much to update
 # TODO: Write database updating section for inserting and updating data
 # TODO: Verify the integrity of the database before and after the update
+# TODO: Complete main logging and run logging
 
 
 # Main function
 def main():
     # Set the current run log filename
-    setup_logging()
+    setup_logs()
+
+    # Create the file for database files checksum
+    flags.create_database_checksum_file()
+
+    # Setup the file to log to
+    setup_main_logfile()
 
     # Wait until change in files is detected and any updates are finished
-    register_change()
+    file_handler.wait_for_new_data()
 
-    # Create a connection to the database
-    log("Database update here")
+    # Copy data directory to temporary location to prevent mid-update changes
+    file_handler.isolate_data()
+
+    # Connect to MySQL server and set the connection as a global variable
+    db_handler.connect_to_database()
+
+    # Ensure proper database and tables exist
+    db_handler.setup_database()
+
+    # Take the new data and load the differences into the database
+    db_handler.run_update()
+
+    # Close the connection to the database
+    db_handler.close_connection()
+
+    # Update the database files checksum stored locally
+    flags.save_database_checksum(config.NEW_CHECKSUM)
+    log("New database checksum saved: %s" % config.NEW_CHECKSUM)
+
+    # Remove temporary database data files
+    file_handler.clean_up()
 
 
 # Main function
 if __name__ == '__main__':
     print("Started: database-manager")
-    time.sleep(20)
+    time.sleep(config.CONTAINER_DELAY)
     try:
         while True:
             main()
