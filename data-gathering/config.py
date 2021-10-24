2ce6157b47acbdcdecd5c145ae7cad5bb43901a1
rol-x <pl.charlessummers@gmail.com>
2021-10-24 13:42:43 +0200

    Global variables rework

--- data-gathering/config.py
+++ data-gathering/config.py
@@ -1,20 +1,22 @@
 """Store and initialize global configuration of the program."""
 
 # Variables for user custom program configuration
-WAIT_COEF = 3.3
-BUTTON_TIMEOUT = 120.0
-EXPANSION_NAME = 'Battlebond'
-MAX_TRIES = 3
 START_FROM = 1
-WEBDRIVER_HOSTNAME = 'firefox_webdriver'
 FORCE_UPDATE = False
+EXPANSION_NAME = 'Battlebond'
+BUTTON_TIMEOUT = 120.0
+WAIT_COEF = 3.3
+MAX_TRIES = 3
 
 # Variables connected to this single run of the code
-LOG_FILENAME = 'other.log'
-DAILY_LOGNAME = 'other_daily.log'
-THIS_DATE_ID = 0
 FILE_PART = 1
+THIS_DATE_ID = 0
+MAIN_LOGNAME = 'other_main.log'
+RUN_LOGNAME = 'other_run.log'
 
 # Fixed variables
+NAME = 'data-gathering'
 BASE_URL = 'https://www.cardmarket.com/en/Magic/Products/Singles/'
 USERS_URL = 'https://www.cardmarket.com/en/Magic/Users/'
+WEBDRIVER_HOSTNAME = 'firefox_webdriver'
+CONTAINER_DELAY = 10
---
+++ database-manager/config.py
@@ -0,0 +1,11 @@
+"""Store and initialize global configuration of the program."""
+
+# Variables connected to this single run of the code
+DATABASE = None
+NEW_CHECKSUM = ''
+MAIN_LOGNAME = 'other_main.log'
+RUN_LOGNAME = 'other_run.log'
+
+# Fixed variables
+NAME = 'database-manager'
+CONTAINER_DELAY = 20
