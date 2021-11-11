be65af36a0f12d36e6131401a6c0550891877f35
rol-x <pl.charlessummers@gmail.com>
2021-11-11 16:45:38 +0100

    Pandas built-in SQL functions used

--- database-manager/config.py
+++ database-manager/config.py
@@ -1,7 +1,7 @@
 """Store and initialize global configuration of the program."""
 
 # Variables connected to this single run of the code
-DATABASE = None
+CONN = None
 NEW_CHECKSUM = ''
 MAIN_LOGNAME = 'other_main.log'
 RUN_LOGNAME = 'other_run.log'
